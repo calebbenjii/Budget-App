1c0c43f529d659742972047c1ce50452778e7765
kaiylebdev <calebben94@gmail.com>
2020-03-12 13:52:10 +0100

    App Module structuring

---
+++ js/app.js
@@ -0,0 +1,36 @@
+
+
+// Data Module
+const budgetController = ( function() {
+
+
+})();
+
+
+
+// UI CONTROLLER
+const UIController = ( function() {
+
+  //Some code
+
+})();
+
+
+// GLOBAL APP CONTROLLER
+const controller = ( function(budgetCtrl, UICtrl) {
+
+  document.querySelector('.add__btn').addEventListener('click', function() {
+
+    // 1. Get the field input data
+
+    // 2. Add the item to the budget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on this UI
+
+  });
+
+})(budgetController, UIController); 
